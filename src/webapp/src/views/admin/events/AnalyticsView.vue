39eefd6b0c4da2bc826a748d157c2ae96ef0198e
Janis Hutz <development@janishutz.com>
2023-06-30 18:59:53 +0200

    add small message to event analytics page

--- src/webapp/src/views/admin/events/AnalyticsView.vue
+++ src/webapp/src/views/admin/events/AnalyticsView.vue
@@ -10,5 +10,7 @@
 <template>
     <div>
         <h2>Event analytics</h2>
+        <p>Get insights into tickets sold, people checked in, available tickets, revenue, etc, in real time!</p>
+        <!-- TODO: Add toggle for real-time update (will fetch every 30s, possibly even using a WS) -->
     </div>
 </template>
