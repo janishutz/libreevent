e767c1fb3d3865a624623af78b932b3713ae5734
Janis Hutz <development@janishutz.com>
2023-10-07 15:04:39 +0200

    update website to be almost complete

---
+++ website/dist/templates/helper.js
@@ -0,0 +1,27 @@
+let theme = localStorage.getItem( 'theme' ) ?? '';
+if ( window.matchMedia( '(prefers-color-scheme: dark)' ).matches || theme === '&#9788;' ) {
+    document.documentElement.classList.add( 'dark' );
+    document.getElementById( 'themeSelector' ).innerHTML = '&#9788;';
+    theme = '&#9788;';
+} else {
+    document.documentElement.classList.add( 'light' );
+    document.getElementById( 'themeSelector' ).innerHTML = '&#9789;';
+    theme = '&#9789;';
+}
+
+// eslint-disable-next-line no-unused-vars
+function changeTheme () {
+    if ( theme === '&#9788;' ) {
+        document.documentElement.classList.remove( 'dark' );
+        document.documentElement.classList.add( 'light' );
+        localStorage.setItem( 'theme', '&#9789;' );
+        document.getElementById( 'themeSelector' ).innerHTML = '&#9789;';
+        theme = '&#9789;';
+    } else if ( theme === '&#9789;' ) {
+        document.documentElement.classList.remove( 'light' );
+        document.documentElement.classList.add( 'dark' );
+        localStorage.setItem( 'theme', '&#9788;' );
+        document.getElementById( 'themeSelector' ).innerHTML = '&#9788;';
+        theme = '&#9788;';
+    }
+}
